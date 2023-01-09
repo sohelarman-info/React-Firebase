c594f6033f579bfd9987878d21207a69b1c79a81
Sohel Arman <sohelarman.info.com>
2023-01-09 22:42:55 +0600

    React Crud with firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Container from "@mui/material/Container";
+import { TextField, Grid, Button } from "@mui/material";
 
 function App() {
+  const [info, setinfo] = useState({
+    fName: "",
+    email: "",
+    deg: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setinfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid item xs={6} className="forms">
+              <TextField
+                onChange={handleChange}
+                type={"text"}
+                id="outlined-basic"
+                label="Username"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="fName"
+              />
+              <TextField
+                onChange={handleChange}
+                type={"email"}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="email"
+              />
+              <TextField
+                onChange={handleChange}
+                type={"text"}
+                id="outlined-basic"
+                label="Designation"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="deg"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className="button"
+                fullWidth
+                size="large"
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </>
   );
 }
 
