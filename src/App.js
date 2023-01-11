5253c3d91e6b75839f79c87e370b49cc4931eaef
Sohel Arman <sohelarman.info.com>
2023-01-11 17:34:07 +0600

    React Crud in firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Container from "@mui/material/Container";
-import { TextField, Grid, Button } from "@mui/material";
+import {
+  TextField,
+  Grid,
+  Button,
+  Icon,
+  SvgIcon,
+  IconButton,
+} from "@mui/material";
+import { getDatabase, ref, set, push, onValue } from "firebase/database";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-  const [info, setinfo] = useState({
+  // UseState
+  const [info, setInfo] = useState({
     fName: "",
     email: "",
     deg: "",
   });
 
+  // input field validation
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [designationError, setDesignationError] = useState("");
+  const [todo, setTodo] = useState([]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setinfo((prev) => ({
+    setInfo((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
+
+  // const validationSchema = () => {
+  //   if (!info.fName) {
+  //     setNameError("Please input your Name!");
+  //   } else if (!info.email) {
+  //     setEmailError("Please input your Email!");
+  //   } else if (!info.deg) {
+  //     setDesignationError("Please input your Designation!");
+  //   }
+  // };
+
+  // Write data in database
+  const db = getDatabase();
+  const handleSubmit = () => {
+    // validationSchema();
+    if (info.fName && info.email && info.deg) {
+      set(push(ref(db, "users")), {
+        fName: info.fName,
+        email: info.email,
+        deg: info.deg,
+      }).then(() => {
+        setInfo({
+          fName: "",
+          email: "",
+          deg: "",
+        });
+      });
+    }
+  };
+
+  // get all data
+
+  useEffect(() => {
+    const starCountRef = ref(db, "users/");
+    onValue(starCountRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((e) => {
+        arr.push(e.val());
+      });
+      setTodo(arr);
+    });
+  }, []);
+
   return (
     <>
       <div>
@@ -33,6 +92,7 @@ function App() {
                 fullWidth
                 name="fName"
               />
+              {nameError ? <p className="user-error">{nameError}</p> : ""}
               <TextField
                 onChange={handleChange}
                 type={"email"}
@@ -43,6 +103,7 @@ function App() {
                 fullWidth
                 name="email"
               />
+              {emailError ? <p className="user-error">{emailError}</p> : ""}
               <TextField
                 onChange={handleChange}
                 type={"text"}
@@ -53,7 +114,13 @@ function App() {
                 fullWidth
                 name="deg"
               />
+              {designationError ? (
+                <p className="user-error">{designationError}</p>
+              ) : (
+                ""
+              )}
               <Button
+                onClick={handleSubmit}
                 variant="contained"
                 color="primary"
                 className="button"
@@ -62,6 +129,23 @@ function App() {
               >
                 Submit
               </Button>
+
+              <div className="box">
+                {todo.map((item, key) => (
+                  <div className="widths" key={key}>
+                    <h4>{item.fName}</h4>
+                    <h5>{item.email}</h5>
+                    <p>{item.deg}</p>
+                    <IconButton
+                      aria-label="delete"
+                      size="large"
+                      className="Delete-btn"
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                ))}
+              </div>
             </Grid>
           </Grid>
         </Container>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5Flk3in2BUheRIjXJRURGN_Wdu5POyHM",
+  authDomain: "react-firebase-c56be.firebaseapp.com",
+  projectId: "react-firebase-c56be",
+  storageBucket: "react-firebase-c56be.appspot.com",
+  messagingSenderId: "33770056235",
+  appId: "1:33770056235:web:a32382a1d23c9e061bd86e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
